a4f903d143633b8ba289c28b57aba667248422be
Christian Montoya <christian.montoya@consensys.net>
2023-03-31 11:56:31 -0400

    So far so good

--- packages/snap/src/index.ts
+++ packages/snap/src/index.ts
@@ -1,33 +1,23 @@
-import { OnRpcRequestHandler } from '@metamask/snaps-types';
-import { panel, text } from '@metamask/snaps-ui';
+import { OnTransactionHandler } from '@metamask/snaps-types';
+import { text } from '@metamask/snaps-ui';
+import { hasProperty, isObject } from '@metamask/utils';
 
 /**
- * Handle incoming JSON-RPC requests, sent through `wallet_invokeSnap`.
+ * Handle an incoming transaction, and return any insights.
  *
  * @param args - The request handler args as object.
- * @param args.origin - The origin of the request, e.g., the website that
- * invoked the snap.
- * @param args.request - A validated JSON-RPC request object.
- * @returns The result of `snap_dialog`.
- * @throws If the request method is not valid for this snap.
+ * @param args.transaction - The transaction object.
+ * @returns The transaction insights.
  */
-export const onRpcRequest: OnRpcRequestHandler = ({ origin, request }) => {
-  switch (request.method) {
-    case 'hello':
-      return snap.request({
-        method: 'snap_dialog',
-        params: {
-          type: 'Confirmation',
-          content: panel([
-            text(`Hello, **${origin}**!`),
-            text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
-          ]),
-        },
-      });
-    default:
-      throw new Error('Method not found.');
+export const onTransaction: OnTransactionHandler = async ({ transaction }) => {
+  if (
+    !isObject(transaction) ||
+    !hasProperty(transaction, 'data') ||
+    typeof transaction.data !== 'string'
+  ) {
+    console.warn('Unknown transaction type.');
+    return { content: text('Unknown transaction') };
   }
+
+  return { content: text('**You are interacting with:** ' + transaction.to) };
 };
